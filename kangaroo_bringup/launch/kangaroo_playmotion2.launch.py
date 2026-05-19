06070b2e19a3c265b0f0e4b8a94bb036a4bcdc84
Aina <aina.irisarri@pal-robotics.com>
2026-05-19 09:46:42 +0200

    No feet_type need it in play_motion2

--- kangaroo_bringup/launch/kangaroo_playmotion2.launch.py
+++ kangaroo_bringup/launch/kangaroo_playmotion2.launch.py
@@ -56,9 +56,6 @@ class LaunchArguments(LaunchArgumentsBase):
     # ["no-arm", "4dof", "5dof", "7dof"]
     arm_type: DeclareLaunchArgument = KangarooArgs.arm_type
 
-    # ["ft-leg", "leg", "no-leg"]
-    # feet_type: DeclareLaunchArgument = KangarooArgs.feet_type
-
     # ["fake-forearm", "ft-gripper", "gripper", "RH8D"]
     end_effector_right: DeclareLaunchArgument = KangarooArgs.end_effector_right
     end_effector_left: DeclareLaunchArgument = KangarooArgs.end_effector_left
@@ -96,7 +93,6 @@ def create_play_motion_filename(context):
     pkg_share_dir = get_package_share_directory(pkg_name)
     end_effector_type = read_launch_argument('end_effector_right', context)
     arm_type = read_launch_argument('arm_type', context)
-    # feet_type = read_launch_argument('feet_type', context)
     has_pelvis = read_launch_argument('has_pelvis', context)
 
     motions_files = []
